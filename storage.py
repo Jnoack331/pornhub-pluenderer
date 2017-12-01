4567a2502de46e54df19a2e8af45a89bc0f9b552
Julius Noack <noack@solutiondrive.de>
2017-12-01 14:48:37 +0100

    test

--- storage.py
+++ storage.py
@@ -16,7 +16,7 @@ class Storage(object):
     def new_file(self, filename):
         if path.exists(filename):
             self._skipped_files.append(filename)
-            raise AlreadyDownloadedError(filename)  #blaasasd
+            raise AlreadyDownloadedError(filename)  #blaasaaa
 
         if path.exists(dl_folder) is not True:
             makedirs(dl_folder)
