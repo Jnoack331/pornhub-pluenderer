606a9f9ab17fe47b3f68252828640c69b340e8a9
Julius Noack <noack@solutiondrive.de>
2017-12-01 12:51:09 +0100

    damaged code to check scruntenizer

--- main.py
+++ main.py
@@ -14,15 +14,3 @@ if __name__ == "__main__":
     viewkeys = ex.get_viewkeys(main_page)
 
     puts(colored.green("starting to download videos."))
-    for key in viewkeys:
-        puts(colored.green("getting video information."))
-        absolute_url = "https://pornhub.com/view_video.php?viewkey=" + key
-        page = dl.get(absolute_url)
-        info = ex.get_video_info(page)
-
-        if info is None:
-            continue
-
-        hdQuality = info['mediaDefinitions'][0]
-        puts(colored.green("downloading video %s." % info['video_title']))
-        dl.save_file(hdQuality["videoUrl"], info['video_title'] + ".mp4")
