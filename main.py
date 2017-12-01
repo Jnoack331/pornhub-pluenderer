d876b721363ee8331504a77c3ac96bd8d4843ff0
Julius Noack <noack@solutiondrive.de>
2017-12-01 12:56:19 +0100

    breaking code for scruntenizer

--- main.py
+++ main.py
@@ -7,10 +7,9 @@ from clint.textui import colored, puts
 if __name__ == "__main__":
     dl = Downloader()
     ex = Extractor()
-    url = "https://pornhub.com"
 
     puts(colored.green("getting video keys."))
-    main_page = dl.get(url)
-    viewkeys = ex.get_viewkeys(main_page)
-
+    puts(colored.green(ex.get_viewkeys(dl.get("https://pornhub.com"))))
     puts(colored.green("starting to download videos."))
+
+
