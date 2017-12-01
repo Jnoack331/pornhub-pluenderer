24ca0f819eb539307a09b0b9023d9bd573e4a335
Julius Noack <noack@solutiondrive.de>
2017-12-01 13:04:46 +0100

    trigger scrunt

--- storage.py
+++ storage.py
@@ -16,7 +16,7 @@ class Storage(object):
     def new_file(self, filename):
         if path.exists(filename):
             self._skipped_files.append(filename)
-            raise AlreadyDownloadedError(filename)  #bla
+            raise AlreadyDownloadedError(filename)  #blaas
 
         if path.exists(dl_folder) is not True:
             makedirs(dl_folder)
